a73b9ae8550b0bd353833b1afd5961643bd992e3
Seesiz <108148026+Seesiz@users.noreply.github.com>
2024-01-13 19:24:43 +0300

    design

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,18 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   isConnected: boolean = true;
+
+  ngOnInit() {
+    const min = 1;
+    const max = 3;
+
+    const randomNumber = Math.floor(Math.random() * (max - min) + min);
+    const imageUrl = `../assets/img/bg_${randomNumber}.jpeg`;
+
+    const image = new Image();
+    image.onload = () => {
+      document.body.style.backgroundImage = `url(${imageUrl})`;
+    };
+    image.src = imageUrl;
+  }
 }
